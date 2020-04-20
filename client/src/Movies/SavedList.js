d56c5f6733e82c24d24e7bf7733636c1d4328de5
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-20 16:01:54 -0700

    added in my function for home prop in savedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home(props) {
+
+}
+return routerHome;
+
 
 const SavedList = props => (
   <div className="saved-list">
